7caf3c00b899f56a40ac660115f25ac9c61c6637
SBats <simon.bats12@gmail.com>
2016-12-12 21:27:38 +0100

    Add missing suffix for selectors

--- src/schema-form/default.widget.ts
+++ src/schema-form/default.widget.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sf-default-field',
-  template: `<p>Cannot find valid type for {{name}}`
+  template: `
+    <p>Cannot find valid type for {{name}}
+  `
 })
 export class DefaultWidget {}
